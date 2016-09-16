d3587f115badc081928c99167af0b40836b6398f
REHAN-PC\Arbisoft <rehanumardogar@gmail.com>
2016-09-16 12:58:37 +0500

    first assignment complete

--- solutions/module1/scripts/apps.js
+++ solutions/module1/scripts/apps.js
@@ -1,8 +1,30 @@
 (function () {
   angular.module('lunchChecker', [])
-  .controller('lunchCheckerController', lunchCheckerController)
+  .controller('lunchCheckerController', lunchCheckerController);
 
-  function lunchCheckerController($scope){
-    $scope.dishes = ''
+  lunchCheckerController.$inject = ['$scope'];
+  function lunchCheckerController ($scope) {
+    $scope.message = '';
+    $scope.dishes = '';
+    $scope.color = 'black';
+
+    function notEmptyDishes(value) {
+      return value != '' && value != ' ';
+    }
+
+    $scope.checkIfTooMuch = function () {
+      var dishesArray = $scope.dishes.split(',');
+      var filteredDishes = dishesArray.filter(notEmptyDishes);
+      if(filteredDishes.length > 0 && filteredDishes.length <= 3) {
+        $scope.message = 'Enjoy!';
+        $scope.color = 'green';
+      } else if (filteredDishes.length > 3) {
+        $scope.message = 'Too much!';
+        $scope.color = 'green';
+      } else {
+        $scope.message = 'Please enter data first';
+        $scope.color = 'red';
+      }
+    }
   }
 })();
