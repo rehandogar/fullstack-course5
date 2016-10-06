e890fd4349d5183b7b295eaac923f96b86eec2fb
REHAN-PC\Arbisoft <rehanumardogar@gmail.com>
2016-10-06 14:31:58 +0500

    assignment 4

--- solutions/module4/scripts/apps.js
+++ solutions/module4/scripts/apps.js
@@ -1,6 +1,22 @@
 (function () {
   "use strict";
-  angular.module('MenuApp', [])
-  .controller('NarrowItDownController', NarrowItDownController);
+  var MenuApp = angular.module('MenuApp', ['ui.router']);
+
+  MenuApp.$inject = ['$stateProvider'];
+  MenuApp.config(function ($stateProvider) {
+    var homeState = {
+      name: 'home',
+      url: '/home',
+      template: 'home.html'
+    }
+    var itemsState = {
+    name: 'items',
+    url: '/items',
+    template: 'items.html'
+  }
+
+  $stateProvider.state(homeState);
+  $stateProvider.state(itemsState);
+  })
 
 })();
